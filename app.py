77e31d3577d91fe3f85bba04e93bfef197e6e692
jinsunwoo <sun_woo__jin@naver.com>
2020-08-20 18:23:01 +0900

    Created readDiary.html and related UI.

--- app.py
+++ app.py
@@ -25,5 +25,11 @@ def main():
 def write():
     return render_template('write.html')
 
+@app.route('/readDiary')
+def readDiary():
+    return render_template('readDiary.html')
+
+
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
